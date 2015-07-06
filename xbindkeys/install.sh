4c75edb091ee4f4403e7c70fffaae3d0c0efd84c
Elliot DeNolf <denolfe@gmail.com>
2015-07-05 18:57:14 -0600

    xbindkeys install script improvements

--- xbindkeys/install.sh
+++ xbindkeys/install.sh
@@ -1,10 +1,19 @@
 DEPS="xbindkeys xbindkeys-config xdotool wmctrl"
 
+success () {
+  printf "\r\033[2K  [ \033[00;32mINSTALLED\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [  \033[0;31mMISSING\033[0m  ] $1\n"
+}
+
 for dep in $DEPS
 do
 	if [ -x "$(command -v $dep)" ]; then
-		echo "$dep installed"
+		success "$dep"
 	else
+		fail "$dep"
 		echo "Installing $dep..."
 		sudo apt-get -qq install $dep
 	fi
