dd0a56d4ac86db5e573f4682428795efb61c1fa4
Elliot DeNolf <denolfe@gmail.com>
2015-07-27 18:30:53 -0600

    Add alias for setting dark or light solarized terminal theme

---
+++ zsh/functions.zsh
@@ -0,0 +1,17 @@
+set_dark () {
+	gsettings set org.pantheon.terminal.settings background '#00002B2B3636'
+	gsettings set org.pantheon.terminal.settings foreground '#838394949696'
+	gsettings set org.pantheon.terminal.settings cursor-color '#838394949696'
+	gsettings set org.pantheon.terminal.settings palette '#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#858599990000:#65657B7B8383:#26268B8BD2D2:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3'
+	gsettings set org.pantheon.terminal.settings opacity 98	
+	gsettings set org.pantheon.terminal.settings follow-last-tab true
+}
+
+set_light() {
+	gsettings set org.pantheon.terminal.settings background '#fdfdf6f6e3e3'
+	gsettings set org.pantheon.terminal.settings foreground '#65657b7b8383'
+	gsettings set org.pantheon.terminal.settings cursor-color '#65657b7b8383'
+	gsettings set org.pantheon.terminal.settings palette '#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3'
+	gsettings set org.pantheon.terminal.settings opacity 98	
+	gsettings set org.pantheon.terminal.settings follow-last-tab true
+}
