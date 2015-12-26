d2bd3b0201f5b0cda7f96545d0bf34589fbaf1b4
Elliot DeNolf <denolfe@gmail.com>
2015-12-25 23:26:05 -0700

    Add workspaces function for elementary os

---
+++ zsh/functions/ws.zsh
@@ -0,0 +1,17 @@
+# Set Elementary OS workspaces
+ws() {
+	help="\n    Usage: ws <num>\n\n    0      Enable Dynamic Workspaces\n    1-9    Set number of workspaces\n"
+	if [ -z "$1" ]
+	then
+		echo $help
+	elif [[ "$1" == "0" ]]; then
+		gsettings set org.pantheon.desktop.gala.behavior dynamic-workspaces true
+		echo "Dynamic workspaces turned on"
+	elif [[ "$1" =~ ^[1-9]+$ ]]; then
+		gsettings set org.pantheon.desktop.gala.behavior dynamic-workspaces false
+		gsettings set org.gnome.desktop.wm.preferences num-workspaces $1
+		echo "Workspaces set to $1"
+	else
+		echo $help
+	fi
+}
