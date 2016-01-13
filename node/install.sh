8cf83c38b0185fbe3da361d8976f2f36b699a427
Elliot DeNolf <denolfe@gmail.com>
2016-01-13 00:12:53 -0700

    Update node scripts

--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,26 @@
 
 source ~/.dotfiles/script/logging
 
+header "Node"
+
+if [ -x "$(command -v npm)" ]; then
+	success npm
+else
+	error npm
+	installing npm
+	sudo apt-get -qq install npm > /dev/null
+	installing_done
+fi
+
+if [ ! -d ~/.nvm ]; then
+	error nvm
+	installing nvm
+	git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+	installing_done
+fi
+source ~/.nvm/nvm.sh
+nvm use system
+
 npm_installed() {
 	if npm list -g $1 | grep $1 --quiet; then
 		success $1
@@ -14,8 +34,6 @@ npm_installed() {
 		success $1
 	fi
 }
-
-header "npm Packages"
 
 for pkg in bower grunt gulp express
 do
