42e73f8cd0c7834811c7b82a333085580eefed83
Elliot DeNolf <denolfe@gmail.com>
2016-01-17 12:22:31 -0700

    Add python install for pip and awscli

---
+++ python/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# 
+# Install pip
+
+source ~/.dotfiles/script/logging
+if [ "$(uname -s)" != "Linux" ];then
+  exit 0
+fi
+
+header "Python"
+
+if [ -x "$(command -v pip)" ]; then
+	success pip
+else
+	error npm
+	installing pip
+	sudo curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
+	sudo -H python /tmp/get-pip.py
+	installing_done
+fi
+
+pip_installed() {
+	if pip list | grep $1 &> /dev/null; then
+		success $1
+	else
+		error $1
+		installing $1
+		sudo -H pip install $1
+		success $1
+	fi
+}
+
+pip_installed "awscli"
