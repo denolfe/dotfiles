5cc4468e9a0521c5bfb828c057b07c1730d31dfb
Elliot DeNolf <denolfe@gmail.com>
2016-01-20 21:56:25 -0700

    Add git editlast

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,5 +5,6 @@
 alias gbv='git branch -v'
 alias gh='git hist'
 
+git config --global alias.editlast 'commit --amend -m' # Make sure to unstage all first!
 git config --global alias.sync '!zsh -ic git-sync'
 git config --global alias.add-upstream '!zsh -ic add-upstream'
