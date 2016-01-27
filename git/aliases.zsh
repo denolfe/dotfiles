15606c41cef5a1f5102dae3f6bf7b35225e48884
Elliot DeNolf <denolfe@gmail.com>
2016-01-26 20:44:41 -0700

    Add git trav to aliases.zsh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,3 +8,4 @@ alias gh='git hist'
 git config --global alias.editlast 'commit --amend -m' # Make sure to unstage all first!
 git config --global alias.sync '!zsh -ic git-sync'
 git config --global alias.add-upstream '!zsh -ic add-upstream'
+git config --global alias.trav '!zsh -ic git-trav'
