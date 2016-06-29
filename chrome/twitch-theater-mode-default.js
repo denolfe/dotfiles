bcc399c445dbe422dd00373164761f3112c09e3b
Elliot DeNolf <denolfe@users.noreply.github.com>
2016-06-29 17:26:59 -0600

    Add viewer count below chat for theater mode

--- chrome/twitch-theater-mode-default.js
+++ chrome/twitch-theater-mode-default.js
@@ -2,7 +2,7 @@
 // @name         Twitch Theater Mode Default
 // @namespace    http://twitch.tv/fathom_
 // @version      0.2
-// @description  Automatically default twitch to theater mode
+// @description  Automatically default twitch to theater mode and add viewer count
 // @author       fathom
 // @match        http://www.twitch.tv/*
 // @match        https://www.twitch.tv/*
@@ -11,6 +11,16 @@
 // ==/UserScript==
 window.addEventListener('load', function() {
   // if stream is live or it is a VOD
-  if ($(".live-count").length > 0 || $(location).attr('href').match(/\/v\//g).length)
+  if ($(".live-count").length > 0 || $(location).attr('href').match(/\/v\//g).length) {
     $("button.player-button--theatre").trigger("click");
+    getViewers();
+  }
 }, false);
+
+function getViewers() {
+  var viewers = $('span.live-count').text().trim();
+  if ($('.new-viewers').length === 0)
+    $('.chat-buttons-container').append("<p class='new-viewers'></p>");
+  $('p.new-viewers').text(viewers + ' viewers');
+}
+setInterval(getViewers, 30 * 1000);
