824b45fc9d22ff418210e5dbd9e5e3060896ca99
Elliot DeNolf <denolfe@gmail.com>
2016-07-31 00:47:15 -0600

    Add rvm install and fix ruby path

---
+++ ruby/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -sSL https://get.rvm.io | bash -s stable
--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-source ~/.rvm/scripts/rvm
-source ~/.rvm/gems/ruby-2.1.4/bin
+source ~/.rvm/bin
