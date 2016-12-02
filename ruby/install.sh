8c7e83e0267447b343f21e4b386e9e80e30c0c1b
Elliot DeNolf <denolfe@gmail.com>
2016-12-02 00:54:25 -0700

    Adjust rvm install script

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+#
+# Install Rvm
 
-curl -sSL https://get.rvm.io | bash -s stable
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash
