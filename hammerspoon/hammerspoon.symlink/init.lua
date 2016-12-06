e1c0626be07aab612634e4228e3da451cc00ec98
Elliot DeNolf <denolfe@gmail.com>
2016-12-05 17:52:19 -0700

    Add chaining and 12x12 grid

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -3,6 +3,9 @@
 
 local hyper = {"shift", "cmd", "alt", "ctrl"}
 hs.window.animationDuration = 0
+hs.grid.setGrid('12x12')
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
 
 require("hs.application")
 require("hs.window")
@@ -11,65 +14,77 @@ require("hs.window")
 -- Window Grid
 -----------------------------------------------
 
-local positions = {
-  maximized = hs.layout.maximized,
-  centered = {x=0.025, y=0.025, w=0.95, h=0.95},
-
-  left34 = {x=0, y=0, w=0.34, h=1},
-  left50 = hs.layout.left50,
-  left66 = {x=0, y=0, w=0.66, h=1},
-  left70 = hs.layout.left70,
-
-  right30 = hs.layout.right30,
-  right34 = {x=0.66, y=0, w=0.34, h=1},
-  right50 = hs.layout.right50,
-  right66 = {x=0.34, y=0, w=0.66, h=1},
-
-  upper50 = {x=0, y=0, w=1, h=0.5},
-  upper50Left50 = {x=0, y=0, w=0.5, h=0.5},
-  upper50Right15 = {x=0.85, y=0, w=0.15, h=0.5},
-  upper50Right30 = {x=0.7, y=0, w=0.3, h=0.5},
-  upper50Right50 = {x=0.5, y=0, w=0.5, h=0.5},
-
-  lower50 = {x=0, y=0.5, w=1, h=0.5},
-  lower50Left50 = {x=0, y=0.5, w=0.5, h=0.5},
-  lower50Right50 = {x=0.5, y=0.5, w=0.5, h=0.5},
-
-  chat = {x=0.5, y=0, w=0.35, h=0.5}
-}
-
 local grid = {
-  {key='q', units={positions.upper50Left50}},
-  {key='w', units={positions.maximized}},
-  {key='e', units={positions.upper50Right50}},
-
-  {key='a', units={positions.left50, positions.left66, positions.left34}},
-  {key='s', units={positions.centered}},
-  {key='d', units={positions.right50, positions.right66, positions.right34}},
-
-  {key='z', units={positions.lower50Left50}},
-  -- {key='x', units={positions.lower50}},
-  {key='c', units={positions.lower50Right50}}
+  topHalf = '0,0 12x6',
+  topThird = '0,0 12x4',
+  topTwoThirds = '0,0 12x8',
+  rightHalf = '6,0 6x12',
+  rightThird = '8,0 4x12',
+  rightTwoThirds = '4,0 8x12',
+  bottomHalf = '0,6 12x6',
+  bottomThird = '0,8 12x4',
+  bottomTwoThirds = '0,4 12x8',
+  leftHalf = '0,0 6x12',
+  leftThird = '0,0 4x12',
+  leftTwoThirds = '0,0 8x12',
+  topLeft = '0,0 6x6',
+  topRight = '6,0 6x6',
+  bottomRight = '6,6 6x6',
+  bottomLeft = '0,6 6x6',
+  fullScreen = '0,0 12x12',
+  centeredBig = '0.5,0.5 11x11',
+  centeredSmall = '4,4 4x4',
 }
-hs.fnutils.each(grid, function(entry)
-  hs.hotkey.bind(hyper, entry.key, function()
-    local units = entry.units
-    local screen = hs.screen.mainScreen()
-    local window = hs.window.focusedWindow()
-    local windowGeo = window:frame()
 
-    local index = 0
-    hs.fnutils.find(units, function(unit)
-      index = index + 1
+local lastSeenChain = nil
+local lastSeenWindow = nil
 
-      local geo = hs.geometry.new(unit):fromUnitRect(screen:frame()):floor()
-      return windowGeo:equals(geo)
-    end)
-    if index == #units then index = 0 end
+-- Chain the specified movement commands.
+--
+-- This is like the "chain" feature in Slate, but with a couple of enhancements:
+--
+--  - Chains always start on the screen the window is currently on.
+--  - A chain will be reset after 2 seconds of inactivity, or on switching from
+--    one chain to another, or on switching from one app to another, or from one
+--    window to another.
+--
+local chain = (function(movements)
+  local chainResetInterval = 2 -- seconds
+  local cycleLength = #movements
+  local sequenceNumber = 1
 
-    currentLayout = null
-    window:moveToUnit(units[index + 1])
-  end)
+  return function()
+    local win = hs.window.frontmostWindow()
+    local id = win:id()
+    local now = hs.timer.secondsSinceEpoch()
+    local screen = win:screen()
+
+    if
+      lastSeenChain ~= movements or
+      lastSeenAt < now - chainResetInterval or
+      lastSeenWindow ~= id
+    then
+      sequenceNumber = 1
+      lastSeenChain = movements
+    elseif (sequenceNumber == 1) then
+      -- At end of chain, restart chain on next screen.
+      screen = screen:next()
+    end
+    lastSeenAt = now
+    lastSeenWindow = id
+
+    hs.grid.set(win, movements[sequenceNumber], screen)
+    sequenceNumber = sequenceNumber % cycleLength + 1
+  end
+end)
+
+
+hs.fnutils.each({
+  { key='q', positions = { grid.leftHalf, grid.leftTwoThirds, grid.topLeft, grid.bottomLeft, grid.leftThird }},
+  { key='w', positions = { grid.fullScreen, grid.centeredBig }},
+  { key='e', positions = { grid.rightHalf, grid.rightTwoThirds, grid.topRight, grid.bottomRight, grid.rightThird }},
+}, function(entry)
+  hs.hotkey.bind(hyper, entry.key, chain(entry.positions))
 end)
 
 -----------------------------------------------
