4563d00248c6645f14fd96d49c0a26b91f1df9a9
Elliot DeNolf <denolfe@gmail.com>
2016-12-17 23:48:36 -0700

    Remove unused zsh functions

--- zsh/functions/e.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Pantheon-files without junk in terminal
-
-# shellcheck disable=SC1073
-e() {
-	pantheon-files "$*" >/dev/null 2>&1 & disown
-	# pantheon-files "$*" >/dev/null 2>&1 &|
-}
