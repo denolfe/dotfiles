33f06bde53cf799b271f599c6697cfad026f4bbe
Elliot DeNolf <denolfe@gmail.com>
2016-12-17 23:50:02 -0700

    Remove a few more unused functions

--- zsh/functions/apt-remove-dep.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-function apt-remove-dep() {
-  sudo aptitude markauto "$(apt-cache showsrc "$1" | sed -e '/Build-Depends/!d;s/Build-Depends: \|,\|([^)]*),*\|\[[^]]*\]//g')"""
-}
-
-alias sard=apt-remove-dep
