a4cb62a139ec2f69f12a403a5c37bd8d62318106
Elliot DeNolf <denolfe@gmail.com>
2016-12-19 18:01:12 -0700

    Add text selection hotkeys

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -1,12 +1,14 @@
 -- Set up
 -----------------------------------------------
 
-local hyper = {'shift', 'cmd', 'alt', 'ctrl'}
-local ultra = {'ctrl','alt','shift'}
 local reloader = require('reloader')
 local windowgrid = require('windowgrid')
 local hotkey = require('hotkey')
 local appfocus = require('appfocus')
+
+-- Modifiers
+local ultra = {'ctrl','alt','shift'} -- Bound to CapsLock in Karabiner-Elements
+local hyper = {'shift', 'cmd', 'alt', 'ctrl'} -- CapsLock + cmd
 
 -----------------------------------------------
 -- Window Grid Binds
@@ -24,9 +26,9 @@ windowgrid.mapbinds(ultra,
 -- Hotkeys
 -----------------------------------------------
 
+-- Vim-style movement
 hotkey.mapbinds(ultra,
   {
-    -- Movement
     { key='h', mod={}, direction='left', shouldRepeat=true },
     { key='j', mod={}, direction='down', shouldRepeat=true },
     { key='k', mod={}, direction='up', shouldRepeat=true },
@@ -37,6 +39,20 @@ hotkey.mapbinds(ultra,
     { key='.', mod={'alt'}, direction='right' }, -- forward word
     -- Rebinds
     { key='delete', mod={}, direction='forwarddelete', shouldRepeat=true } -- forward delete
+  }
+)
+-- Text Selection
+hotkey.mapbinds(hyper,
+  {
+    -- Highlight
+    { key='h', mod={'shift'}, direction='left', shouldRepeat=true },
+    { key='j', mod={'shift'}, direction='down', shouldRepeat=true },
+    { key='k', mod={'shift'}, direction='up', shouldRepeat=true },
+    { key='l', mod={'shift'}, direction='right', shouldRepeat=true },
+    { key='n', mod={'shift','cmd'}, direction='left' },  -- beginning of line
+    { key='p', mod={'shift','cmd'}, direction='right' }, -- end of line
+    { key='m', mod={'shift','alt'}, direction='left' },  -- back word
+    { key='.', mod={'shift','alt'}, direction='right' }, -- forward word
   }
 )
 
