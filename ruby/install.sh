5c1ac42d5c280c69179030461af748ff8c3f30f1
Elliot DeNolf <denolfe@gmail.com>
2017-03-12 22:51:21 -0600

    Add gnupg2 to ruby install script

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,5 +2,9 @@
 #
 # Install Rvm
 
+if ! test "$(which gpg)"; then
+  brew install gnupg2
+fi
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash
