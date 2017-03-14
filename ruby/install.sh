26a6a36100be0aacd72fe2f5354b0bc6c110e605
Elliot DeNolf <denolfe@gmail.com>
2017-03-14 00:06:44 -0600

    Update ruby script with better logging

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,9 +2,19 @@
 #
 # Install Rvm
 
+source "$(dirname "$0")"/../_script/logging
+
+header "Ruby"
+
 if ! test "$(which gpg)"; then
-  brew install gnupg2
+  brew install gnupg2 &> /dev/null
 fi
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash
+if test "$(which rvm)"; then
+  success rvm
+else
+  installing rvm
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  curl -sSL https://get.rvm.io | bash
+  installing_done
+fi
