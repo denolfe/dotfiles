129a840a8ae3538e8b42055a5d2e00feb02d2a9b
Elliot DeNolf <denolfe@gmail.com>
2018-06-12 16:51:08 -0600

    Move to the Export dropdown

--- chrome/jira-copy-story-details.js
+++ chrome/jira-copy-story-details.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        Jira Copy Details
-// @version      0.2
-// @description Add buttons to copy story details as text or markdown
+// @name        Jira Copy Story Details
+// @version     0.3
+// @description Add options to the Export dropdown to copy story details as text or markdown
 // @namespace   Violentmonkey Scripts
 // @author      Elliot DeNolf
 // @match       *://jira.nordstrom.net/browse/*
@@ -12,14 +12,13 @@
 (function () {
   'use strict';
 
-  var createButton = function (buttonText) {
-    var button = document.createElement('button');
-    button.innerHTML = buttonText;
-    button.className += " aui-button";
-    button.className += " aui-button-primary";
-    button.className += " aui-style";
-    button.setAttribute("style", "margin:5px 10px 0 -5px;");
-    return button;
+  var createLi = function (text) {
+    var li = document.createElement('li');
+    li.className = "aui-list-item";
+    var a = document.createElement('a');
+    a.innerHTML = text;
+    li.append(a);
+    return li;
   }
 
   var setClipAndNotify = function (text) {
@@ -27,34 +26,31 @@
     GM_notification("Text Copied:\n" + text);
   }
 
-  var breadcrumbs = document.getElementsByClassName("aui-nav")[0];
+  var exportDropdown = document.querySelector("#viewissue-export_drop > ul");
 
-  if (breadcrumbs !== undefined) {
+  if (exportDropdown !== undefined) {
     var currentUrl = window.location.href;
     var title = document.getElementById('summary-val').innerText;
     var urlSplit = currentUrl.split(/\//);
     var storyNumber = urlSplit[urlSplit.length - 1]
 
-    var li = document.createElement('li');
-    var button = createButton('Copy');
-    button.onclick = function () {
+
+    var detailsButton = createLi('Copy Details');
+    detailsButton.onclick = function () {
       var textToCopy = storyNumber + ": " + title;
       setClipAndNotify(textToCopy);
       return false;
     };
-    li.appendChild(button);
 
-    var li2 = document.createElement("li");
-    var mdButton = createButton('Copy md');
+    var mdButton = createLi('Copy Markdown');
     mdButton.onclick = function () {
       var urlSplit = currentUrl.split(/\//);
       var md = "[" + storyNumber + ": " + title + "](" + currentUrl + ")";
       setClipAndNotify(md);
       return false;
     };
-    li2.appendChild(mdButton);
 
-    breadcrumbs.append(li);
-    breadcrumbs.append(li2);
+    exportDropdown.append(detailsButton);
+    exportDropdown.append(mdButton);
   }
 })();
