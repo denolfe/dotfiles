ca67556ddc00344cb2ec4c495fbd7029c8359678
Elliot DeNolf <denolfe@gmail.com>
2018-06-12 10:29:17 -0600

    Refactor jira link copy script

--- chrome/jira-copy-link.js
+++ chrome/jira-copy-link.js
@@ -1,49 +1,57 @@
 // ==UserScript==
 // @name        Jira Copy Details
-// @version      0.1
-// @description Add buttons to copy link and also markdown text
+// @version      0.2
+// @description Add buttons to copy story details as text or markdown
 // @namespace   Violentmonkey Scripts
 // @author      Elliot DeNolf
 // @match       *://jira.nordstrom.net/browse/*
 // @grant       GM_setClipboard
+// @grant       GM_notification
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
-  var breadcrumbs = document.getElementsByClassName("aui-nav-breadcrumbs")[0];
+  var createButton = function (buttonText) {
+    var button = document.createElement('button');
+    button.innerHTML = buttonText;
+    button.className += " aui-button";
+    button.className += " aui-button-primary";
+    button.className += " aui-style";
+    button.setAttribute("style", "margin:5px 10px 0 -5px;");
+    return button;
+  }
 
-  if (breadcrumbs !== undefined)
-  {
+  var setClipAndNotify = function (text) {
+    GM_setClipboard(text);
+    GM_notification("Text Copied:\n" + text);
+  }
+
+  var breadcrumbs = document.getElementsByClassName("aui-nav")[0];
+
+  if (breadcrumbs !== undefined) {
     var currentUrl = window.location.href;
     var title = document.getElementById('summary-val').innerText;
     var urlSplit = currentUrl.split(/\//);
     var storyNumber = urlSplit[urlSplit.length - 1]
 
-    var li = document.createElement("li");
-    var button = document.createElement('button');
-    button.innerHTML = 'Copy Details';
-    button.onclick = function(){
-      GM_setClipboard(storyNumber + ": " + title);
+    var li = document.createElement('li');
+    var button = createButton('Copy');
+    button.onclick = function () {
+      var textToCopy = storyNumber + ": " + title;
+      setClipAndNotify(textToCopy);
       return false;
     };
-    button.className += " aui-button";
-    button.className += " aui-button-primary";
-    button.className += " aui-style";
     li.appendChild(button);
 
     var li2 = document.createElement("li");
-    var mdButton = document.createElement('button');
-    mdButton.innerHTML = 'Copy md';
-    mdButton.onclick = function(){
+    var mdButton = createButton('Copy md');
+    mdButton.onclick = function () {
       var urlSplit = currentUrl.split(/\//);
-      var md = "["+ storyNumber + ": " + title +"]("+ currentUrl +")";
-      GM_setClipboard(md);
+      var md = "[" + storyNumber + ": " + title + "](" + currentUrl + ")";
+      setClipAndNotify(md);
       return false;
     };
-    mdButton.className += " aui-button";
-    mdButton.className += " aui-button-primary";
-    mdButton.className += " aui-style";
     li2.appendChild(mdButton);
 
     breadcrumbs.append(li);
