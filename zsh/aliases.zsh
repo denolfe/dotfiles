54cc56af23b2dc2a7746247ee9bf18b151feb320
Elliot DeNolf <denolfe@gmail.com>
2018-07-03 01:12:57 -0400

    Clean up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@
 alias rl='source ~/.zshrc; echo ".zshrc reloaded"'
 alias regen='zgen reset;source ~/.zshrc'
 
-alias ls='ls -GFHp'
 alias ll='ls -la'
 
 # Easier navigation
@@ -22,7 +21,6 @@ alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| grep'
 alias -g NUL='&> /dev/null'
-alias -g C='| pbcopy'
 alias -g J='| python -m json.tool'
 
 # Prompt if overwriting
@@ -37,32 +35,28 @@ alias .f='cd ~/.dotfiles'
 ff(){ find . -type f -name "$1" ;}
 fd(){ find . -type d -name "$1" ;}
 
-if [ "$(uname -s)" = "Linux" ];then
-	alias ls='ls --color=auto -p'
-	alias sagi='sudo apt-get install'
-	alias sagu='sudo apt-get update'
-	alias saar='sudo add-apt-repository'
-	alias sagr='sudo apt-get remove'
-	alias -g C='| xclip -selection c'
-  alias pbcopy='xclip -selection c'
-	alias pbpaste='xclip -selection clipboard -o'
-	alias rsx='sudo killall xbindkeys && sudo xbindkeys && echo "xbindkeys restarted"'
-fi
+# Detect the platform (similar to $OSTYPE)
+OS="`uname`"
+case $OS in
+  'Linux')
+		alias ls='ls --color=auto -p'
+		alias sagi='sudo apt-get install'
+		alias sagu='sudo apt-get update'
+		alias saar='sudo add-apt-repository'
+		alias sagr='sudo apt-get remove'
+		alias -g C='| xclip -selection c'
+		alias pbcopy='xclip -selection c'
+		alias pbpaste='xclip -selection clipboard -o'
+    ;;
+  'Darwin')
+		alias -g C='| pbcopy'
+    ;;
+  *) ;;
+esac
 
 # Other bash stuff
-alias mkcd='mkc'
-alias sta='st . -a'
-alias sto='sublo'
-
-# Apps
 alias t="touch"
-alias ds="dashing start"
-alias djs="dashing-js start"
-alias be="bundle exec"
-alias mm="middleman"
-alias djs="dashing-js start"
-alias subla="subl . -a"
-
+alias cat="pygmentize -g"
 
 # Zshmarks ==> Bashmarks
 alias g="jump"
