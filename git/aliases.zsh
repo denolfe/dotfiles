68b196d3167654e036cc262aede04b22c3afa7de
Elliot DeNolf <denolfe@gmail.com>
2018-07-13 23:17:19 -0400

    Add more git aliases and sort

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,13 +2,24 @@
 #
 # Some additional git aliases
 
-alias gbv='git branch -v'
-alias gau='git add -u'
-alias gh='git hist'
-alias gcof='git checkout -f'
-alias groot='cd $(git rev-parse --show-toplevel)'
+alias gau="git add -u"
+alias gbd="git branch -D"
+alias gbm="git branch --merged"
+alias gbnm="git branch --no-merged"
+alias gbv="git branch -v"
+alias gbv="git for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) (%(color:green)%(committerdate:relative)%(color:reset))'"
+alias gcane="git commit --amend --no-edit"
+alias gcof="git checkout -f"
+alias gdlast="git diff HEAD~1..HEAD"
+alias gh="git hist"
+alias glast="git log -1 HEAD"
+alias gpdel="git push --delete"
+alias groot="cd $(git rev-parse --show-toplevel)"
+alias gstu="git status -uno"
+alias gundo="git reset HEAD~1"
+alias guns="git reset HEAD --"
 
-git config --global alias.editlast 'commit --amend -m' # Make sure to unstage all first!
-git config --global alias.sync '!zsh -ic git-sync'
-git config --global alias.add-upstream '!zsh -ic add-upstream'
-git config --global alias.trav '!zsh -ic git-trav'
+git config --global alias.editlast "commit --amend -m" # Make sure to unstage all first!
+git config --global alias.sync "!zsh -ic git-sync"
+git config --global alias.add-upstream "!zsh -ic add-upstream"
+git config --global alias.trav "!zsh -ic git-trav"
