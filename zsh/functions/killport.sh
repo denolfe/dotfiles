270d75aa80a6c07c7b628bcb60099f807d7b12e7
Elliot DeNolf <denolfe@gmail.com>
2019-08-24 21:48:12 -0400

    Move scripts to dedicated directory. Links to ~/bin

--- zsh/functions/killport.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Kill process using specific port
-
-killport() {
-	fuser -k "$1"/tcp &> /dev/null
-}
--- zsh/functions/pathadd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pathadd() {
-    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-        PATH="${PATH:+"$PATH:"}$1"
-    fi
-}
