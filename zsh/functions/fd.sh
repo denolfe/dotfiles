d950064f093b9c650e74400b7e4b2cfb7479bdae
Elliot DeNolf <denolfe@gmail.com>
2019-12-26 12:26:14 -0500

    Add fd function

---
+++ zsh/functions/fd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Another fd - cd into the selected directory
+# This one differs from the above, by only showing the sub directories and not
+#  showing the directories within those.
+fd() {
+  DIR=`find * -maxdepth 0 -type d -print 2> /dev/null | fzf-tmux` \
+    && cd "$DIR"
+}
