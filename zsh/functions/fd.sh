8e9d7c70ca4115bb8550e2229bdb2e354b114a0f
Elliot DeNolf <denolfe@gmail.com>
2019-12-27 00:04:14 -0500

    More fd script adjustments

--- zsh/functions/fd.sh
+++ zsh/functions/fd.sh
@@ -4,6 +4,6 @@
 # This one differs from the above, by only showing the sub directories and not
 #  showing the directories within those.
 fd() {
-  DIR=`find * -maxdepth 0 -type d -print 2> /dev/null | fzf-tmux` \
+  DIR=`find * -maxdepth 0 -type d -print 2> /dev/null | fzf` \
     && cd "$DIR"
 }
