a8222e2d0309496b1649f8544a5d57e4ee6bee73
Elliot DeNolf <denolfe@gmail.com>
2019-12-29 15:40:39 -0500

    Consolidate fcom and fcoc into one, switches on fzf expect ctrl-o

--- zsh/functions/fcom.sh
+++ zsh/functions/fcom.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+# Interactive git checkout specific commit
+# Ctrl-o to perform checkout of currently selected commit
 fcom() {
-  local commits commit
+  local commits commit out key
   commits=$(git log --pretty=format:"%h %s (%cr) <%an>" --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
-  echo $commit
+  out=$(echo "$commits" | fzf --tac +s +m -e --expect=ctrl-o)
+  key=$(head -1 <<< "$out")
+  commit=$(sed 1d <<< "$out")
+  if [ "$key" = ctrl-o ]; then
+    git checkout $(echo "$commit" | sed "s/ .*//")
+  else
+    echo "$commit"
+  fi
 }
