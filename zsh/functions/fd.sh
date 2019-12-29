86fc311d63a032ffa928498aa74c6b001b5edcc1
Elliot DeNolf <denolfe@gmail.com>
2019-12-29 11:56:57 -0500

    Add optional depth parameter to fd

--- zsh/functions/fd.sh
+++ zsh/functions/fd.sh
@@ -4,6 +4,10 @@
 # This one differs from the above, by only showing the sub directories and not
 #  showing the directories within those.
 fd() {
-  DIR=`find * -maxdepth 0 -type d -print 2> /dev/null | fzf` \
+  depth="$1"
+  if [ -z "$1" ]; then
+    depth=0
+  fi
+  DIR=`find * -maxdepth $depth -type d -print 2> /dev/null | fzf` \
     && cd "$DIR"
 }
