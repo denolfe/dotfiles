3eab6349e827e6218df28c5afbb2015baab1ea7e
Elliot DeNolf <denolfe@gmail.com>
2020-01-20 08:40:24 -0500

    Adjust colors for fcom

--- zsh/functions/fcom.sh
+++ zsh/functions/fcom.sh
@@ -4,8 +4,8 @@
 # Ctrl-o to perform checkout of currently selected commit
 fcom() {
   local commits commit out key
-  commits=$(git log --pretty=format:"%h %s (%cr) <%an>" --abbrev-commit --reverse) &&
-  out=$(echo "$commits" | fzf --tac +s +m -e --header "Ctrl-o to checkout commit" --expect=ctrl-o)
+  commits=$(git log --color=always --pretty=format:"%C(auto)%h %s %C(bold)(%cr)%Creset <%an>" --abbrev-commit --reverse) &&
+  out=$(echo "$commits" | fzf --ansi --tac +s +m -e --header "-- Ctrl-o to checkout commit --" --expect=ctrl-o)
   key=$(head -1 <<< "$out")
   commit=$(sed 1d <<< "$out")
   if [ "$key" = ctrl-o ]; then
