ff7817de39582f63134e3170c6579096a92f4b09
Elliot DeNolf <denolfe@gmail.com>
2020-03-04 10:50:04 -0500

    feat(hs): remove movement hotkeys, karabiner now used

--- macos/hammerspoon/init.lua
+++ macos/hammerspoon/init.lua
@@ -22,41 +22,7 @@ windowgrid.mapbinds(ultra,
   }
 )
 
------------------------------------------------
--- Hotkeys
------------------------------------------------
-
--- Vim-style movement
-hotkey.mapbinds(ultra,
-  {
-    { key='h', mod={}, direction='left', shouldRepeat=true },
-    { key='j', mod={}, direction='down', shouldRepeat=true },
-    { key='k', mod={}, direction='up', shouldRepeat=true },
-    { key='l', mod={}, direction='right', shouldRepeat=true },
-    { key='n', mod={'cmd'}, direction='left' },  -- beginning of line
-    { key='p', mod={'cmd'}, direction='right' }, -- end of line
-    { key='m', mod={'alt'}, direction='left' },  -- back word
-    { key='.', mod={'alt'}, direction='right' }, -- forward word
-    -- Rebinds
-    { key='delete', mod={}, direction='forwarddelete', shouldRepeat=true } -- forward delete
-  }
-)
--- Text Selection
-hotkey.mapbinds(hyper,
-  {
-    -- Highlight
-    { key='h', mod={'shift'}, direction='left', shouldRepeat=true },
-    { key='j', mod={'shift'}, direction='down', shouldRepeat=true },
-    { key='k', mod={'shift'}, direction='up', shouldRepeat=true },
-    { key='l', mod={'shift'}, direction='right', shouldRepeat=true },
-    { key='n', mod={'shift','cmd'}, direction='left' },  -- beginning of line
-    { key='p', mod={'shift','cmd'}, direction='right' }, -- end of line
-    { key='m', mod={'shift','alt'}, direction='left' },  -- back word
-    { key='.', mod={'shift','alt'}, direction='right' }, -- forward word
-  }
-)
-
-hs.hotkey.bind(ultra, 'r', function() 
+hs.hotkey.bind(ultra, 'r', function()
   reloader.reload(true)
 end)
 
