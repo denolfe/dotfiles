58e3d00664df3244cad152eee83045bb22636224
Elliot DeNolf <denolfe@gmail.com>
2020-03-18 14:06:32 -0400

    chore(git): move functions into separate file

--- aliases/git.aliases.sh
+++ aliases/git.aliases.sh
@@ -25,38 +25,3 @@ alias gstu="git status -uno"
 alias gundo="git reset HEAD~1"
 alias guns="git reset HEAD --"
 
-# Add upstream remote and set tracking of master
-grau() {
-  git remote add upstream "$1" || return 1
-  git fetch upstream || return 1
-  git branch -u upstream/master
-}
-
-# Stash changes, get latest master, re-apply
-getlatest() {
-  local stash_string=$(LC_CTYPE=C tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32 | xargs)
-  git stash push -m $stash_string
-  git checkout master
-  git pull
-  if [[ $(git stash list | head -n1) == *$stash_string* ]]; then
-    git stash apply
-  fi
-}
-
-# Show previously used conventional commit scopes
-gscopes() {
-  local output=$(git log --pretty=oneline --abbrev-commit --no-merges | grep "):" | cut -d "(" -f2 | cut -d ")" -f1)
-  if [[ "$1" == "-c" ]];then
-    output=$(echo $output | sort | uniq -c | sort -rn | sed "s/^/  /")
-  else
-    output=$(echo $output | sort | uniq | sed "s/^/  /")
-  fi
-  echo -e "\033[96m\033[1mPrevious commit scopes:\033[0m\n"
-  echo $output
-}
-
-# Diff commits with tracked remote branch
-gdremote() {
-  remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
-  git log $remote_branch..HEAD --format='%C(bold cyan)%h%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset%C(yellow)%d%Creset'
-}
---
+++ functions/git.functions.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Git helper functions
+
+# Add upstream remote and set tracking of master
+grau() {
+  git remote add upstream "$1" || return 1
+  git fetch upstream || return 1
+  git branch -u upstream/master
+}
+
+# Stash changes, get latest master, re-apply
+getlatest() {
+  local stash_string=$(LC_CTYPE=C tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32 | xargs)
+  git stash push -m $stash_string
+  git checkout master
+  git pull
+  if [[ $(git stash list | head -n1) == *$stash_string* ]]; then
+    git stash apply
+  fi
+}
+
+# Show previously used conventional commit scopes
+gscopes() {
+  local output=$(git log --pretty=oneline --abbrev-commit --no-merges | grep "):" | cut -d "(" -f2 | cut -d ")" -f1)
+  if [[ "$1" == "-c" ]];then
+    output=$(echo $output | sort | uniq -c | sort -rn | sed "s/^/  /")
+  else
+    output=$(echo $output | sort | uniq | sed "s/^/  /")
+  fi
+  echo -e "\033[96m\033[1mPrevious commit scopes:\033[0m\n"
+  echo $output
+}
+
+# Diff commits with tracked remote branch
+gdremote() {
+  remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+  git log $remote_branch..HEAD --format='%C(bold cyan)%h%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset%C(yellow)%d%Creset'
+}
+
