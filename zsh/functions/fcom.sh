5a63c5fcef5cc4c8d55897b9b24d1bac08995938
Elliot DeNolf <denolfe@gmail.com>
2020-03-19 17:00:32 -0400

    feat(func): fcom function no longer show merges

--- zsh/functions/fcom.sh
+++ zsh/functions/fcom.sh
@@ -4,7 +4,7 @@
 # Ctrl-o to perform checkout of currently selected commit
 fcom() {
   local commits commit out key
-  commits=$(git log --color=always --pretty=format:"%C(auto)%h %s %C(bold)(%cr)%Creset <%an>" --abbrev-commit --reverse) &&
+  commits=$(git log --no-merges --color=always --pretty=format:"%C(auto)%h %s %C(bold)(%cr)%Creset <%an>" --abbrev-commit --reverse) &&
   out=$(echo "$commits" | fzf --ansi --tac +s +m -e --header "-- Ctrl-o to checkout commit --" --expect=ctrl-o)
   key=$(head -1 <<< "$out")
   commit=$(sed 1d <<< "$out")
