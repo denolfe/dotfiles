3f73d72cf27eb1bd00ee78fd73ac23bc449bf58b
Elliot DeNolf <denolfe@gmail.com>
2020-08-05 16:36:45 -0400

    feat(zsh): remove ssh-connect

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -24,7 +24,6 @@ if ! zgen saved; then
     zgen load caarlos0/zsh-add-upstream
     zgen load denolfe/zsh-travis
     zgen load denolfe/zsh-prepend
-    zgen load gko/ssh-connect
 
     zgen load agkozak/zsh-z
     zgen load andrewferrier/fzf-z
