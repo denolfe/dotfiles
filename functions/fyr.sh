1556a49242fb7eb4f88ed401b91313f27631da17
Elliot DeNolf <denolfe@gmail.com>
2020-08-10 14:56:19 -0400

    feat(func): change fnpr to fyr, use yarn

--- functions/fyr.sh
+++ functions/fyr.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-# Interactive npm run script
-fnpr() {
+fyr() {
   package=$(git rev-parse --show-toplevel)/package.json
   if [[ ! -f "$package" ]];then
     echo "No package.json found"
     return 1
   fi
-  cat "$package" | \
-  jq -r '.scripts | to_entries | map("\(.key)\t\(.value|tostring)")|.[]' | \
+  jq -r '.scripts | to_entries | map("\(.key)\t\(.value|tostring)")|.[]' "$package" | \
   awk -F'\t' '{printf "%-15s %-30s\n", $1, $2}' | \
   fzf --height 30% | \
   sed 's/ .*//' | \
-  xargs npm run
+  xargs yarn
 }
