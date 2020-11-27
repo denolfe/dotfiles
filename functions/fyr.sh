6f7f6d6ba56b309ac10b10d40902d1f6225f4664
Elliot DeNolf <denolfe@gmail.com>
2020-11-27 08:30:52 -0500

    feat(node): fyr looks for relative package.json before git root, add ynuke alias

--- functions/fyr.sh
+++ functions/fyr.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 fyr() {
-  package=$(git rev-parse --show-toplevel)/package.json
-  if [[ ! -f "$package" ]];then
+  relative_package=package.json
+
+  if [[ -f "$relative_package" ]]; then
+    package="$relative_package"
+  else
+    package=$(git rev-parse --show-toplevel)/package.json
     echo "No package.json found"
     return 1
   fi
+
   jq -r '.scripts | to_entries | map("\(.key)\t\(.value|tostring)")|.[]' "$package" | \
   awk -F'\t' '{printf "%-15s %-30s\n", $1, $2}' | \
   fzf --height 30% | \
