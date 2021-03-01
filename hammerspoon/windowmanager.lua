08798567378d12a8ba3bb829fc76a337380d604e
Elliot DeNolf <denolfe@gmail.com>
2021-02-28 21:44:37 -0500

    feat(hs): add ultrawide layout

--- hammerspoon/windowmanager.lua
+++ hammerspoon/windowmanager.lua
@@ -1,8 +1,3 @@
-hs.window.animationDuration = 0
-hs.grid.setGrid('12x12')
-hs.grid.MARGINX = 0
-hs.grid.MARGINY = 0
-
 -- Cycle through positions on multiple presses
 local lastSeenChain = nil
 local lastSeenWindow = nil
@@ -36,34 +31,61 @@ local chain = (function(movements)
   end
 end)
 
-local grid = {
-  top50 = '0,0 12x6',
-  top33 = '0,0 12x4',
-  top66 = '0,0 12x8',
-  right50 = '6,0 6x12',
-  right40 = '7,0 5x12',
-  right33 = '8,0 4x12',
-  right66 = '4,0 8x12',
-  bottom50 = '0,6 12x6',
-  bottom33 = '0,8 12x4',
-  bottom66 = '0,4 12x8',
-  left50 = '0,0 6x12',
-  left33 = '0,0 4x12',
-  left60 = '0,0 7x12',
-  left66 = '0,0 8x12',
-  topLeft = '0,0 6x6',
-  topLeft60 = '0,0 7x6',
-  topRight = '6,0 6x6',
-  topRight40 = '7,0 5x6',
-  bottomRight = '6,6 6x6',
-  bottomRight40 = '7,6 6x6',
-  bottomLeft = '0,6 6x6',
-  bottomLeft60 = '0,6 7x6',
-  full = '0,0 12x12',
-  centeredBig = '0.5,0.5 11x11',
-  centeredMedium = '2,1 8x10',
-  centeredSmall = '2.5,1.5 7x9'
-}
+hs.window.animationDuration = 0
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+local mainScreen = hs.screen.primaryScreen():name()
+local grid = nil
+if (mainScreen == 'LC49G95T') then
+  hs.grid.setGrid('24x12')
+  grid = {
+    middle1='7,0 10x12',
+    middle2='8,0 8x12',
+    left1='0,0 7x12',
+    left2='0,0 8x12',
+    left50 = '0,0 12x12',
+    right1='17,0 7x12',
+    right2='16,0 8x12',
+    right50 = '12,0 12x12',
+  }
+
+  hs.hotkey.bind(ultra, 'q', chain({ grid.left1, grid.left2, grid.left50 }))
+  hs.hotkey.bind(ultra, 'w', chain({ grid.middle1, grid.middle2  }))
+  hs.hotkey.bind(ultra, 'e', chain({ grid.right1, grid.right2, grid.right50 }))
+else
+  hs.grid.setGrid('12x12')
+  grid = {
+    top50 = '0,0 12x6',
+    top33 = '0,0 12x4',
+    top66 = '0,0 12x8',
+    right50 = '6,0 6x12',
+    right40 = '7,0 5x12',
+    right33 = '8,0 4x12',
+    right66 = '4,0 8x12',
+    bottom50 = '0,6 12x6',
+    bottom33 = '0,8 12x4',
+    bottom66 = '0,4 12x8',
+    left50 = '0,0 6x12',
+    left33 = '0,0 4x12',
+    left60 = '0,0 7x12',
+    left66 = '0,0 8x12',
+    topLeft = '0,0 6x6',
+    topLeft60 = '0,0 7x6',
+    topRight = '6,0 6x6',
+    topRight40 = '7,0 5x6',
+    bottomRight = '6,6 6x6',
+    bottomRight40 = '7,6 6x6',
+    bottomLeft = '0,6 6x6',
+    bottomLeft60 = '0,6 7x6',
+    full = '0,0 12x12',
+    centeredBig = '0.5,0.5 11x11',
+    centeredMedium = '2,1 8x10',
+    centeredSmall = '2.5,1.5 7x9'
+  }
+  hs.hotkey.bind(ultra, 'q', chain({ grid.left60, grid.left50, grid.topLeft60, grid.bottomLeft60, grid.topLeft, grid.bottomLeft }))
+  hs.hotkey.bind(ultra, 'w', chain({ grid.full, grid.centeredBig, grid.centeredMedium, grid.centeredSmall }))
+  hs.hotkey.bind(ultra, 'e', chain({ grid.right40, grid.right50, grid.topRight40, grid.bottomRight40, grid.topRight, grid.bottomRight }))
+end
 
 -- Source: https://stackoverflow.com/a/58662204/1717697
 function moveToNextScreen()
@@ -73,8 +95,4 @@ function moveToNextScreen()
   win:move(win:frame():toUnitRect(currentScreen:frame()), currentScreen:next(), true, 0)
 end
 
--- Binds
-hs.hotkey.bind(ultra, 'q', chain({ grid.left60, grid.left50, grid.topLeft60, grid.bottomLeft60, grid.topLeft, grid.bottomLeft }))
-hs.hotkey.bind(ultra, 'w', chain({ grid.full, grid.centeredBig, grid.centeredMedium, grid.centeredSmall }))
-hs.hotkey.bind(ultra, 'e', chain({ grid.right40, grid.right50, grid.topRight40, grid.bottomRight40, grid.topRight, grid.bottomRight }))
 hs.hotkey.bind(ultra, 'tab', moveToNextScreen)
