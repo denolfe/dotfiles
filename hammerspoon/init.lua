b2a74de4cc2eb914e1e2600dc5c0c6ebd2b71b66
Elliot DeNolf <denolfe@gmail.com>
2021-12-26 22:52:07 -0500

    feat(hs): add launch or toggle iterm

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,20 +5,32 @@ hyperCmd = {'shift', 'cmd', 'alt', 'ctrl'} -- CapsLock + cmd
 -- Chained binds for window resizing and movement
 require('windowmanager')
 
--- App shortcuts
-local bindApp = function(appName)
+local launchOrFocus = function(appName)
   return function()
     hs.application.launchOrFocus(appName)
   end
 end
 
-hs.hotkey.bind(hyper, ';', bindApp('iTerm'))
-hs.hotkey.bind(hyper, 'g', bindApp('Google Chrome'))
-hs.hotkey.bind(hyper, 'c', bindApp('Visual Studio Code'))
-hs.hotkey.bind(hyper, 'r', bindApp('Notion'))
-hs.hotkey.bind(hyper, 'f', bindApp('Slack'))
-hs.hotkey.bind(hyper, 'v', bindApp('Spotify'))
-hs.hotkey.bind(hyper, 't', bindApp('Microsoft Outlook'))
+-- Launch or focus app, hide if already frontmost
+local launchOrFocusToggle = function(appName)
+  return function()
+    local appInstance = hs.application.find(appName)
+    local isFrontmost = appInstance and appInstance:isFrontmost()
+    if isFrontmost then
+      appInstance:hide()
+    else
+      hs.application.launchOrFocus(appName)
+    end
+  end
+end
+
+hs.hotkey.bind(hyper, ';', launchOrFocus('iTerm'))
+hs.hotkey.bind(hyper, 'g', launchOrFocus('Google Chrome'))
+hs.hotkey.bind(hyper, 'c', launchOrFocus('Visual Studio Code'))
+hs.hotkey.bind(hyper, 'r', launchOrFocus('Notion'))
+hs.hotkey.bind(hyper, 'f', launchOrFocus('Slack'))
+hs.hotkey.bind(hyper, 'v', launchOrFocus('Spotify'))
+hs.hotkey.bind(hyper, 't', launchOrFocus('Microsoft Outlook'))
 
 -- Spotify shortcuts
 hs.hotkey.bind(hyper, '\\', hs.spotify.playpause)
