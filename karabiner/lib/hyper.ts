668d3926955643b36970ef65db0bced525967377
Elliot DeNolf <denolfe@gmail.com>
2022-01-02 23:48:21 -0500

    feat(karabiner): refactor hyper bind functions

--- karabiner/lib/hyper.ts
+++ karabiner/lib/hyper.ts
@@ -1,8 +1,6 @@
 import {
   Key,
   Manipulator,
-  KeyPressTo,
-  KeyPressFrom,
 } from 'https://raw.githubusercontent.com/esamattis/deno_karabiner/master/lib/karabiner.ts'
 
 const hyperMods: Key[] = ['left_control', 'left_shift', 'left_option']
@@ -12,43 +10,29 @@ const hyperCmdMods: Key[] = ['left_control', 'left_shift', 'left_option', 'left_
  * CapsLock + Key
  */
 export function hyper(from: Key, to: Key, options?: { modifiers: Key[] }): Manipulator {
-  return bind(
-    { key_code: from },
-    {
-      key_code: to,
-      ...(options?.modifiers && { modifiers: options?.modifiers }),
-    },
-    hyperMods,
-  )
+  return bind(from, to, hyperMods, options)
 }
 
 /**
  * CapsLock + Cmd + Key
  */
 export function hyperCmd(from: Key, to: Key, options?: { modifiers: Key[] }): Manipulator {
-  return bind(
-    { key_code: from },
-    {
-      key_code: to,
-      ...(options?.modifiers && { modifiers: options?.modifiers }),
-    },
-    hyperCmdMods,
-  )
+  return bind(from, to, hyperCmdMods, options)
 }
 
-function bind(from: KeyPressFrom, to: KeyPressTo, mods: Key[]): Manipulator {
+function bind(from: Key, to: Key, mods: Key[], options?: { modifiers: Key[] }): Manipulator {
   return {
     type: 'basic',
     from: {
-      key_code: from.key_code,
+      key_code: from,
       modifiers: {
         mandatory: mods,
       },
     },
     to: [
       {
-        key_code: to.key_code,
-        modifiers: to?.modifiers,
+        key_code: to,
+        ...(options?.modifiers && { modifiers: options?.modifiers }),
       },
     ],
   }
