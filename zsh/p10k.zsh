f5f92d8bb198992e6b9231196e5df0e7c954fdda
Elliot DeNolf <denolfe@gmail.com>
2022-03-25 17:25:09 -0400

    feat(zsh): turn off transient prompt

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -1672,7 +1672,7 @@
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=always
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
 
   # Instant prompt mode.
   #
