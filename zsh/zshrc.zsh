a19adcd9968c7c4a399a156cecfe9f2102647f94
Elliot DeNolf <denolfe@gmail.com>
2022-03-30 00:02:49 -0400

    feat(fzf): customize pointer

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -114,7 +114,7 @@ if type fd > /dev/null 2>&1; then
 fi
 
 # FZF config and theme
-export FZF_DEFAULT_OPTS='--reverse --bind 'ctrl-l:cancel' --height=90%'
+export FZF_DEFAULT_OPTS='--reverse --bind 'ctrl-l:cancel' --height=90% --pointer='▶''
 source $DOTFILES/zsh/fzf-theme.sh
 export FZF_TMUX_HEIGHT=80%
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
