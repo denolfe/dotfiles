4b2f7dc4816d35c27ad649ad01995cf2d1e34c15
Elliot DeNolf <denolfe@gmail.com>
2022-05-27 14:02:36 -0400

    feat(func): add quick cd script for dev and sandbox

---
+++ functions/cdf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Quick cd into specified dir, fzf picker
+cdf() {
+  local dir=$1
+  local exa_cmd='exa -alF --icons --color=always --group-directories-first  --no-user --no-permissions --no-filesize --no-time'
+  repo=$(find "$dir" -type d -maxdepth 1 -mindepth 1 | awk -F'/' '{print $NF}' | sort | \
+fzf --preview "$exa_cmd $dir/{}" --preview-window 70%)
+  cd "$dir"/"$repo" || return 1
+}
+
+# Quick cd into ~/dev
+dev() {
+  cdf ~/dev
+}
+
+# Quick cd into ~/dev/sandbox
+sb() {
+  cdf ~/dev/sandbox
+}
