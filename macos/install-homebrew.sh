5ac5f0478c9aa3b63361699ea52138e0b4a85a7a
Elliot DeNolf <denolfe@gmail.com>
2022-05-29 09:59:12 -0400

    feat(dotbot): install homebrew via dotbot install

---
+++ macos/install-homebrew.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Install homebrew if it's not already installed
+
+if ! type brew > /dev/null 2>&1; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
