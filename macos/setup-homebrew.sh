8ee57e6293844d7dbfcd08b07dbd849dad5a689f
Elliot DeNolf <denolfe@gmail.com>
2022-06-02 14:29:49 -0400

    feat(dotbot): remove dotbot-brew, add script to replace

---
+++ macos/setup-homebrew.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Install homebrew and essential packages
+
+if ! type brew > /dev/null 2>&1; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_UPGRADE=1
+
+PACKAGES=(
+  "asdf"
+  "fzf"
+  "git"
+  "exa"
+  "neovim"
+  "tree"
+  "fd"
+  "jq"
+  "ripgrep"
+  "zsh"
+)
+
+echo "Checking Homebrew packages..."
+
+brew_list=$(brew list --formulae -1)
+
+for PKG in "${PACKAGES[@]}"
+do
+  if ! echo "$brew_list" | grep -q "$PKG"; then
+    echo "Installing $PKG..."
+    brew install $PKG
+  fi
+done
+
+echo "All packages installed."
