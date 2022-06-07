21dac4b32ffb2bd94aef0cd68031c690bd3849fb
Elliot DeNolf <denolfe@gmail.com>
2022-06-06 23:26:24 -0400

    feat(func): no extension in cws workspace name

--- functions/cws.sh
+++ functions/cws.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+#
+# 'Choose workspace'
+# Shows a workspace chooser using fzf
 
 cws() {
-  local ws_dir
+  local ws_dir ext
   ws_dir="$HOME"/dev
-  find "$ws_dir"/*.code-workspace -exec basename {} \; | \
-  fzf --height 30% --prompt "Select Workspace >" --preview-window 75% --preview "bat $ws_dir/{} | jq -r '.folders[].path' | sort" | \
-  xargs -I {} code "$ws_dir"/{}
+  ext=".code-workspace"
+  find $ws_dir/*$ext -exec basename {} \; | \
+  sed -r "s/$ext//" | \
+  fzf --height 30% --prompt "Select Workspace >" --preview-window 75% --preview "bat $ws_dir/{}.code-workspace | jq -r '.folders[].path' | sort" | \
+  xargs -I {} code $ws_dir/{}$ext
 }
