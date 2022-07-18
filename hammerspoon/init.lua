6768f686194454cc6b42cbcfc6922a3d28992b23
Elliot DeNolf <denolfe@gmail.com>
2022-07-18 13:38:08 -0700

    feat(vs): cycle apps between slack and discord

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,13 +12,24 @@ local bindApp = function(appName)
   end
 end
 
+local cycleApps = function(appName, appName2)
+  return function()
+    local currentApp = hs.application.frontmostApplication():name()
+    if (currentApp ~= appName) then
+      hs.application.launchOrFocus(appName)
+    else
+      hs.application.launchOrFocus(appName2)
+    end
+  end
+end
+
 hs.hotkey.bind(hyper, ';', bindApp('iTerm'))
 hs.hotkey.bind(hyper, 'g', bindApp('Google Chrome'))
 hs.hotkey.bind(hyper, 'c', bindApp('Visual Studio Code'))
 hs.hotkey.bind(hyper, 'r', bindApp('Notion'))
-hs.hotkey.bind(hyper, 'f', bindApp('Slack'))
+hs.hotkey.bind(hyper, 'f', cycleApps('Slack', 'Discord'))
 hs.hotkey.bind(hyper, 'v', bindApp('Spotify'))
-hs.hotkey.bind(hyper, 't', bindApp('Microsoft Outlook'))
+-- hs.hotkey.bind(hyper, 't', bindApp(''))
 
 -- Spotify shortcuts
 hs.hotkey.bind(hyper, '\\', hs.spotify.playpause)
