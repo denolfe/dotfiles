22765436ca81ed4a08417117816fe45ccda1852a
Elliot DeNolf <denolfe@gmail.com>
2022-08-18 21:29:11 -0400

    feat(phoenix): implement app launch or focus binds

--- phoenix/src/phoenix.ts
+++ phoenix/src/phoenix.ts
@@ -11,6 +11,13 @@ console.log('Phoenix Started')
 const HYPER: Phoenix.ModifierKey[] = ['ctrl', 'shift', 'option']
 const HYPER_CMD: Phoenix.ModifierKey[] = [...HYPER, 'cmd']
 
+bindApp(';', HYPER, 'iTerm')
+bindApp('g', HYPER, 'Google Chrome')
+bindApp('c', HYPER, 'Visual Studio Code')
+bindApp('r', HYPER, 'Notion')
+bindApp('v', HYPER, 'Spotify')
+bindCycleApps('f', HYPER, ['Slack', 'Discord'])
+
 Key.on(
   'w',
   HYPER,
@@ -21,10 +28,6 @@ Key.on(
     { x: 2.5, y: 1.5, w: 7, h: 9 },
   ]),
 )
-
-Key.on('tab', HYPER, () => {
-  moveToNextScreen()
-})
 
 // Cycle 2-window split, left side primary
 Key.on(
@@ -51,3 +54,50 @@ Key.on(
     splitWindowLayout.right['4x12'],
   ]),
 )
+
+Key.on('tab', HYPER, () => {
+  moveToNextScreen()
+})
+
+/**
+ * Launch or focus last app window
+ */
+function bindApp(
+  key: string,
+  mods: Phoenix.ModifierKey[],
+  appName: string,
+  appLaunchName?: string,
+) {
+  Key.on(key, mods, () => {
+    const app = App.get(appName)
+    if (!app) {
+      App.launch(appLaunchName ?? appName, { focus: true })
+    } else if (
+      Window.focused()?.app().bundleIdentifier() === app.bundleIdentifier()
+    ) {
+      // focus second to last window
+      app.windows()[1].focus() // TODO: Make this cycle all app windows
+    } else {
+      app.mainWindow()?.focus() // last used window
+    }
+  })
+}
+
+/**
+ * Cycle between 2 apps
+ */
+function bindCycleApps(
+  key: string,
+  mods: Phoenix.ModifierKey[],
+  appNames: string[],
+) {
+  Key.on(key, mods, () => {
+    const apps = appNames.map((a) => App.get(a)).filter(Boolean)
+    const currentApp = App.focused().name()
+    if (currentApp === apps[0]?.name()) {
+      apps[1]?.focus() || App.launch(appNames[1], { focus: true })
+    } else {
+      apps[0]?.focus() || App.launch(appNames[0], { focus: true })
+    }
+  })
+}
