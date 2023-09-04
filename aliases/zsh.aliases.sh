3dc7251a054665d120648f3450e61b1ea48de950
Elliot DeNolf <denolfe@gmail.com>
2023-09-03 23:06:18 -0400

    feat(alias): CDD renamed to cdd

--- aliases/zsh.aliases.sh
+++ aliases/zsh.aliases.sh
@@ -77,7 +77,8 @@ if type bat > /dev/null 2>&1; then
   alias cat="bat"
 fi
 
-alias CDD='cd $(ls -v1td */ | head -1)'
+# cd into the most recently modified directory
+alias cdd='cd $(ls -v1td */ | head -1)'
 alias to_lower="tr '[:upper:]' '[:lower:]'"
 alias to_upper="tr '[:lower:]' '[:upper:]'"
 
