b3028220c4f3af28493e8ef80887abdec2c50405
Elliot DeNolf <denolfe@gmail.com>
2024-09-07 14:23:06 -0400

    fix(vs): prevent code command from creating new icon in dock

--- functions/code.override.sh
+++ functions/code.override.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 if command -v code >/dev/null 2>&1; then
-	code() {
-		case "$1" in
-		save-ext)
-			echo "Saving code extensions..."
+  code() {
+    case "$1" in
+    save-ext)
+      echo "Saving code extensions..."
       code --list-extensions > $DOTFILES/vscode/extensions.txt
-			;;
-		install-ext)
-			echo "Installing code extensions..."
+      ;;
+    install-ext)
+      echo "Installing code extensions..."
       cat $DOTFILES/vscode/extensions.txt | xargs -L 1 code --install-extension
-			;;
-		*)
-			command code "$@"
-			;;
-		esac
-	}
+      ;;
+    *)
+      # Prevent additional vs code icon in dock
+      # https://github.com/microsoft/vscode/issues/60579#issuecomment-434583718
+      open -b com.microsoft.VSCode "$@"
+      ;;
+    esac
+  }
 fi
