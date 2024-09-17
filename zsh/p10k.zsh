6edd4288b3f7feafbb62ee88ade62495f666f17e
Elliot DeNolf <denolfe@gmail.com>
2024-09-16 21:07:40 -0400

    feat(zsh): update branch icon

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -365,7 +365,8 @@
   typeset -g POWERLEVEL9K_VCS_GIT_ICON=''
 
   # Branch icon. Set this parameter to '\uF126 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
+  # typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
