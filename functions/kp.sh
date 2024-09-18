443aa184948b52b413d0629be068e7640cedebf5
Elliot DeNolf <denolfe@gmail.com>
2024-09-18 18:59:43 -0400

    feat(func): kp improvements

--- functions/kp.sh
+++ functions/kp.sh
@@ -6,16 +6,16 @@
 # kp [port|process_name]
 
 kp() {
-  ARG=$1
+  ARG="$1"
 
   if [ -z "$ARG" ]; then
-    echo "Please provide either a port number or a process name."
+    echo "Please provide port number or process name."
     return 1
   fi
 
   if [[ "$ARG" =~ ^[0-9]+$ ]]; then
     # Argument is a port number
-    PIDS=$(lsof -i :$ARG -t)
+    PIDS=$(lsof -i :"$ARG" -t)
     if [ -z "$PIDS" ]; then
       echo "No process found running on port $ARG."
       echo ""
@@ -23,10 +23,11 @@ kp() {
     fi
 
     echo "Killing processes on port $ARG:"
-    for PID in $(echo $PIDS); do
-      PROCESS_NAME=$(ps -p $PID -o comm=)
-      echo "  Killing process $PROCESS_NAME (PID: $PID)"
-      kill -9 $PID
+    # shellcheck disable=SC2116
+    for PID in $(echo "$PIDS"); do
+      PROCESS_NAME=$(ps -p "$PID" -o comm=)
+      echo "  Killing $PROCESS_NAME (PID: $PID)"
+      kill -9 "$PID"
     done
 
     echo ""
@@ -42,10 +43,11 @@ kp() {
     fi
 
     echo "Killing processes with name '$ARG':"
-    for PID in $(echo $PIDS); do
-      PROCESS_NAME=$(ps -p $PID -o comm=)
+    # shellcheck disable=SC2116
+    for PID in $(echo "$PIDS"); do
+      PROCESS_NAME=$(ps -p "$PID" -o comm=)
       echo "  Killing process $PROCESS_NAME (PID: $PID)"
-      kill -9 $PID
+      kill -9 "$PID"
     done
 
     echo ""
