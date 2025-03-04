47e8fa8108bba154922847dfac35d1a6dec3e782
Elliot DeNolf <denolfe@gmail.com>
2025-03-04 12:32:46 -0500

    feat(zsh): fzf style tweaks

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -126,18 +126,19 @@ fi
 # FZF config and theme
 export FZF_DEFAULT_OPTS="--reverse \
 --bind 'ctrl-l:cancel' \
---bind 'ctrl-/:toggle-preview' \
+--bind 'ctrl-p:toggle-preview' \
 --height=90% \
 --info inline-right \
 --highlight-line \
---pointer ▓ \
+--pointer ▌ \
 --prompt '▌ ' \
 --marker ▏ \
---padding 1,2"
+--padding 1,2 \
+--wrap-sign '↳ '"
 
 source $DOTFILES/zsh/fzf-theme-dark-plus.sh
 
-export FZF_CTRL_R_OPTS="--preview 'echo {} | bat --color=always --style=snip --language=zsh' --preview-window up:5:hidden:wrap"
+export FZF_CTRL_R_OPTS="--preview 'echo {} | bat --color=always --style=snip --language=zsh' --wrap-sign '↳ ' --preview-window up:5:hidden:wrap --bind 'ctrl-p:toggle-preview'"
 
 export FZF_TMUX_HEIGHT=80%
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
