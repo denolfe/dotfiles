bb61a9f583ea22ebac04f630db53034617e82351
Elliot DeNolf <denolfe@gmail.com>
2025-05-02 13:55:50 -0400

    feat(alias): adjust cpj and cpjs output

--- aliases/node.aliases.sh
+++ aliases/node.aliases.sh
@@ -19,8 +19,8 @@ alias ynuke='rm -f yarn.lock && rm -rf node_modules && yarn'
 alias la='lerna add'
 alias lad='lerna add --dev'
 
-alias cpj='cat package.json | jless'
-alias cpjs='cat package.json | jq -r ".scripts" | jless'
+alias cpj='cat package.json | jless -N -m line'
+alias cpjs='cat package.json | jq -r ".scripts" | jless -N -m line'
 alias cpjv='cat package.json | jq -r ".version"'
 
 # Reverse pnpm aliases set by pnpm plugin
