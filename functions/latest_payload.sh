228f2324fbd5239e945081f982ec86441b1aab30
Elliot DeNolf <denolfe@gmail.com>
2025-09-21 22:07:01 -0400

    feat(func): update latest_payload

--- functions/latest_payload.sh
+++ functions/latest_payload.sh
@@ -2,8 +2,8 @@
 
 # Installs latest payload using create-payload-app and cd into the project directory
 latest_payload() {
-  PROJECT_DIR=payload-$(pnpm view payload dist-tags --json | jq -r '.beta' | tr '.' '-')
+  PROJECT_DIR=payload-$(pnpm view payload dist-tags.latest | tr '.' '-')
   echo "Project dir: $PROJECT_DIR"
-  pnpx create-payload-app@beta -n "$PROJECT_DIR" -t blank --db mongodb --db-connection-string "mongodb://127.0.0.1/$PROJECT_DIR" --yes
+  pnpx create-payload-app@latest -n "$PROJECT_DIR" -t blank --db mongodb --db-connection-string "mongodb://127.0.0.1/$PROJECT_DIR" --yes
   cd ./"$PROJECT_DIR" || return
 }
