96abdec43120237b8bf820c89ffec19bd6a10bc9
Elliot DeNolf <denolfe@gmail.com>
2026-06-12 15:44:31 -0400

    feat(vim): remove spell check

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,4 +5,4 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
